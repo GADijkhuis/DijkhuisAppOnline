1962ae075d0d948a415ce231e6209546d7cb20f9
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2026-01-05 20:15:22 +0100

    Deploy version 0.1.2 of Dijkhuis App Online

--- PublishSite.sh
+++ PublishSite.sh
@@ -1,8 +1,6 @@
 git init
 git remote add origin https://github.com/GADijkhuis/DijkhuisAppOnline.git
-git checkout main
-git pull
 git add .
 git commit -m "Deploy version 0.1.2 of Dijkhuis App Online"
 git branch -M main
-git push -u origin main
+git push -u origin main --force
